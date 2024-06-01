46d74de8a8e6d9224e64f8d4087b5979a0a48bf2
Biagio Gennuso <gennuso.biagio@gmail.com>
2024-06-01 15:47:35 +0200

    Creato endpoint registrazione

---
+++ pages/api/register.js
@@ -0,0 +1,26 @@
+import connectDB from '../../lib/mongodb';
+import User from '../../models/User';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, password, name } = req.body;
+
+    await connectDB();
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'Email already exists' });
+      }
+
+      const user = new User({ email, password, name });
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
